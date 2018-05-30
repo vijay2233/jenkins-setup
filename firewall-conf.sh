848c9ccc0bb95ebcd756951a6660dd2273b8c100
Chandra <chandu.2035@gmail.com>
2018-05-30 05:20:54 -0400

    changed code to test

--- firewall-conf.sh
+++ firewall-conf.sh
@@ -9,9 +9,9 @@
 
 enable_firewall_port()
 {
- sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
- sudo firewall-cmd --reload
+ sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp >/dev/null 2>&1
+
+ echo -e "\nOpening firewall port 8080"
+ sudo firewall-cmd --reload >/dev/null 2>&1
+ echo -e "\nReloaded firewall rules..."
 }
-
-
-
